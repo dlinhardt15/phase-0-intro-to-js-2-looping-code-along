67f602517ddf96c65154807d3035bff0dc387e6d
Danny Linhardt <daniel.linhardt@gmail.com>
2022-05-19 09:47:54 -0400

    commit tree 871e0727b275a87916cc7619ca740757b5ee776d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const newNames = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newNames;
+}
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    } 
+}
